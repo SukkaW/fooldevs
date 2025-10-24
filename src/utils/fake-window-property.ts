a9113e695c590eaa4876d7b3cbbe33221d1118e3
SukkaW <isukkaw@gmail.com>
2025-10-24 23:38:55 +0800

    fix: do not break anything

--- src/utils/fake-window-property.ts
+++ src/utils/fake-window-property.ts
@@ -2,6 +2,8 @@ import { noop } from 'foxts/noop';
 
 const noobj = {};
 
+const noValueDefault = Symbol('no value default');
+
 export function fakeWindowProperty(name: string, value: unknown = noobj) {
   if (typeof window === 'undefined') {
     return noop;
@@ -11,15 +13,27 @@ export function fakeWindowProperty(name: string, value: unknown = noobj) {
     return noop;
   }
 
+  // track new value set by userland
+  let newValue = noValueDefault;
+
   Object.defineProperty(window, name, {
     enumerable: false,
     configurable: true,
     get() {
+      if (newValue !== noValueDefault) {
+        return newValue;
+      }
       return value;
+    },
+    set(v) {
+      newValue = v;
     }
   });
   return () => {
+    // we only clean up our fake property if userland didn't set a new value
+    if (newValue === noValueDefault) {
     // eslint-disable-next-line @typescript-eslint/no-dynamic-delete -- safe since we already know we set it
-    delete window[name as keyof Window];
+      delete window[name as keyof Window];
+    }
   };
 }
