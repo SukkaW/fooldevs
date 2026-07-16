e6cb42543e4374107e44130fad792bb520e6ae7d
SukkaW <isukkaw@gmail.com>
2026-07-16 16:53:59 +0800

    chore: housekeeping

--- src/modules/corejs.ts
+++ src/modules/corejs.ts
@@ -3,8 +3,7 @@ import type { FoolModule } from './_types';
 import { fakeWindowProperty } from '../utils/fake-window-property';
 import { core_js as version } from './_version.json';
 
-const corejs: FoolModule = () => fakeWindowProperty('__core-js_shared__', { versions: [{ version }]
-});
+const corejs: FoolModule = () => fakeWindowProperty('__core-js_shared__', { versions: [{ version }] });
 
 if (process.env.LATEST_VERSION_SCRIPT === 'true') {
   corejs.npm = 'core-js';
